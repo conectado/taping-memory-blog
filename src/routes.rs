5b717a332b00e54010eac72499fb4077ca597e3f
conectado <gabrielalejandro7@gmail.com>
2020-08-21 00:25:38 -0300

    Fix router bug where article view was catched by router

--- src/routes.rs
+++ src/routes.rs
@@ -2,7 +2,7 @@ use yew_router::prelude::*;
 
 #[derive(Switch, Clone)]
 pub enum AppRoute {
-    #[to = "/articles/{post_name}"]
+    #[to = "/#articles/{post_name}"]
     ViewPost(String),
     #[to = "/"]
     List,
